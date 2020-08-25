276e11397755327552a1ecfcd3faf90e567063d5
Jen Niles <51378247+jenfloods@users.noreply.github.com>
2020-08-25 09:28:29 -0500

    Update setup.php
    Putting back to original

--- lib/setup.php
+++ lib/setup.php
@@ -135,7 +135,7 @@ function display_sidebar() {
  * Theme assets
  */
 function assets() {
-  wp_enqueue_style('sage/css', Assets\asset_path('styles/main.css'), true, null);
+  wp_enqueue_style('sage/css', Assets\asset_path('styles/main.css'), false, null);
 
   if (is_single() && comments_open() && get_option('thread_comments')) {
     wp_enqueue_script('comment-reply');
